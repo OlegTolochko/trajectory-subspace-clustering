0e455384e131c1e4d00413329eeb8be0a03e0db7
Oleg Tolochko <oleg.i.tolochko@gmail.com>
2025-05-04 15:33:58 +0200

    Introduce the possiblity of using different clustering algorithms during evaluation

--- inference.py
+++ inference.py
@@ -2,7 +2,7 @@ import torch
 from models.trajectory_embedder import TrajectoryEmbeddingModel
 from training import Hopkins155
 from torch.utils.data import DataLoader
-from sklearn.cluster import AgglomerativeClustering
+from sklearn.cluster import AgglomerativeClustering, KMeans, SpectralClustering
 from sklearn.metrics.cluster import contingency_matrix
 from scipy.optimize import linear_sum_assignment
 import numpy as np
@@ -10,7 +10,7 @@ import numpy as np
 
 def load_model():    
     model = TrajectoryEmbeddingModel()
-    load_path = 'out/models/trained_model_weights_normalized4.pt'
+    load_path = 'out/models/trained_model_weights_normalized.pt'
 
     target_device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     state_dict = torch.load(load_path, map_location=target_device)
@@ -42,7 +42,7 @@ def load_trajectory_data():
     loaded_data = DataLoader(dataset, batch_size=1)
     return loaded_data
 
-def evaluate_model_performance(model):
+def evaluate_model_performance(model, cluster_algo_name='hierarchical'):
     data = load_trajectory_data()
     individual_error_rates = []
     target_device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -60,21 +60,41 @@ def evaluate_model_performance(model):
             
             k = sequence['num_clusters'].item()
             clusters = AgglomerativeClustering(n_clusters=k, linkage='ward', compute_distances=False)
-            predicted_labels = clusters.fit_predict(f)
+            
+            predicted_labels = None
+            if cluster_algo_name == 'hierarchical':
+                clusters = AgglomerativeClustering(n_clusters=k, linkage='ward', compute_distances=False)
+                predicted_labels = clusters.fit_predict(f)
+            elif cluster_algo_name == 'kmeans':
+                clusters = KMeans(n_clusters=k, random_state=0, n_init=10)
+                predicted_labels = clusters.fit_predict(f)
+            elif cluster_algo_name == 'spectral':
+                # tested options: 'rbf', 'nearest_neighbor'; possibly worth experminenting with different hyp. params here
+                clusters = SpectralClustering(n_clusters=k, random_state=0, affinity='rbf', n_neighbors=20)
+                predicted_labels = clusters.fit_predict(f)
+            else:
+                print(f"Error: Unknown clustering algorithm '{cluster_algo_name}'")
+                continue
+
             error_rate = calculate_clustering_error(seq_labels_gt.numpy(), predicted_labels)
             individual_error_rates.append(error_rate)
 
     mean_error_rate = sum(individual_error_rates) / len(individual_error_rates)
     median_error_rate = np.median(individual_error_rates)
-    print(f"\nEvaluation Complete.")
+    print(f"Evaluation Complete with {cluster_algo_name} clustering:")
     print(f"Mean Clustering Error: {mean_error_rate * 100:.2f}%")
-    print(f"Median Clustering Error: {median_error_rate * 100:.2f}%")
+    print(f"Median Clustering Error: {median_error_rate * 100:.2f}% \n")
 
     return mean_error_rate
 
+def compare_all_clustering_methods(model):
+    algorithms = ['hierarchical', 'kmeans', 'spectral']
+    for algorithm in algorithms:
+        evaluate_model_performance(model,algorithm)
+
 def main():
     model = load_model()
-    error_rate = evaluate_model_performance(model)
+    error_rate = compare_all_clustering_methods(model)
 
 if __name__ == '__main__':
     main()
