20e1e12c5b49d8d0d7c717de4d333db906ae5408
Oleg Tolochko <oleg.i.tolochko@gmail.com>
2025-06-25 12:30:46 +0200

    Write new training loop to perform unsupervised model training

---
+++ training_unsupervised.py
@@ -0,0 +1,303 @@
+from datetime import datetime
+import os
+import copy
+
+import torch
+import torch.optim as optim
+from torch.utils.data import DataLoader, Subset
+import torch.nn.functional as F
+from torch.optim.lr_scheduler import ExponentialLR
+from tqdm import tqdm
+import wandb
+
+from models.trajectory_embedder import TrajectoryEmbeddingModel
+from losses import L_FeatDiff, L_InfoNCE, L_Residual, L_orthogonal, L_InfoNCE_unsupervised
+from datasets import augment_normalized_data, load_dataset, get_train_val_loaders, randomly_augment_seq
+from inference import evaluate_model_performance
+
+
+def train_model_unsupervised(config, model_save_path="./out/models/"):
+    metrics_to_log = {
+        "total_loss": 0,
+        "infonce_loss": 0,
+        "residual_loss": 0,
+        "feat_diff_loss": 0,
+        f"mean_clustering_error": 0,
+    }
+
+    main_dataset = load_dataset(config["train_data"])
+    main_train_loader, main_val_loader = get_train_val_loaders(
+        main_dataset,
+        val_split_size=config["validation_split"],
+        strict_sequence_train_val_split=config["strict_sequence_train_val_split"],
+        include_partial_sequences_train=config["include_partial_sequences_train"],
+        include_partial_sequences_val=config["include_partial_sequences_val"],
+    )
+    additional_val_loader = None
+    if config["additional_val_data"]:
+        metrics_to_log.update(
+            {f"{str.lower(config['additional_val_data'])}_mean_clustering_error": 0}
+        )
+        additional_val_dataset = load_dataset(config["additional_val_data"])
+        additional_val_loader = DataLoader(
+            additional_val_dataset, batch_size=config["batch_size"], shuffle=True
+        )
+
+    device = torch.device(config["device"])
+
+    model = TrajectoryEmbeddingModel(alph0=config["alph0"])
+    model = model.to(device)
+
+    optimizer_stage1 = optim.Adam(
+        model.feature_extractor.parameters(),
+        lr=config["learning_rate"],
+        weight_decay=config["weight_decay"],
+    )
+    optimizer_stage2 = optim.Adam(
+        model.parameters(),
+        lr=config["learning_rate"],
+        weight_decay=config["weight_decay"],
+    )
+    scheduler_stage1 = ExponentialLR(optimizer_stage1, gamma=config["scheduler_gamma"])
+    scheduler_stage2 = ExponentialLR(optimizer_stage2, gamma=config["scheduler_gamma"])
+
+    torch.backends.cudnn.benchmark = True
+
+    pretrained_model = pretraining_loop(
+        config=config,
+        model=model,
+        train_loader=main_train_loader,
+        device=device,
+        optimizer=optimizer_stage1,
+        scheduler=scheduler_stage1,
+    )
+
+    pretrained_model = pretrained_model.to(device)
+
+    timestamp = datetime.now().strftime("%Y%m%dT%H%M%S")
+    if wandb.run:
+        wandb_id = wandb.run.id
+    else:
+        wandb_id = 0
+    model_name = f"{config['model_name']}-{timestamp}-{wandb_id}.pth"
+
+    fully_trained_model = full_training_loop(
+        config=config,
+        model=pretrained_model,
+        train_loader=main_train_loader,
+        val_loader=main_val_loader,
+        additional_val_loader=additional_val_loader,
+        device=device,
+        optimizer=optimizer_stage2,
+        scheduler=scheduler_stage2,
+        metrics=metrics_to_log,
+        model_name=model_name,
+    )
+
+    model_save_path = os.path.join(model_save_path, model_name)
+    torch.save(fully_trained_model.state_dict(), model_save_path)
+    print(f"Saved the model to {model_save_path}.")
+
+    return fully_trained_model
+
+
+def pretraining_loop(config, model, train_loader, device, optimizer, scheduler):
+    for epoch in tqdm(range(config["pretraining_epochs"]), desc="Pretraining Model"):
+        model = model.to(device)
+        epoch_loss_stage1 = 0.0
+        num_seq_processed = 0
+        model.feature_extractor.train()
+        model.subspace_estimator.eval()
+        for batch_data in train_loader:
+            seq = batch_data["trajectories"].to(device).squeeze(0)
+            seq_aug1 = randomly_augment_seq(seq)
+            seq_aug2 = randomly_augment_seq(seq)
+
+            optimizer.zero_grad()
+            mask1 = (torch.rand_like(seq[..., :1], device=device) > 0.25)
+            mask2 = (torch.rand_like(seq[..., :1], device=device) > 0.25)
+            seq_aug1 = seq_aug1 * mask1
+            seq_aug2 = seq_aug2 * mask2
+            seq_aug1_permuted = seq_aug1.permute(0, 2, 1)
+            seq_aug2_permuted = seq_aug2.permute(0, 2, 1)
+            
+            
+            f1 = model.feature_extractor(seq_aug1_permuted)
+            f2 = model.feature_extractor(seq_aug2_permuted)
+
+
+            loss = L_InfoNCE_unsupervised(f1, f2)
+            loss.backward()
+            optimizer.step()
+            epoch_loss_stage1 += loss.item()
+            num_seq_processed += 1
+
+        scheduler.step()
+
+        avg_loss = (
+            epoch_loss_stage1 / num_seq_processed if num_seq_processed > 0 else 0.0
+        )
+
+        print(
+            f"Pretraining Epoch {epoch + 1}/{config['pretraining_epochs']}, Avg Loss: {avg_loss:.4f}"
+        )
+
+    return model
+
+
+def full_training_loop(
+    config,
+    model,
+    train_loader,
+    val_loader,
+    additional_val_loader,
+    device,
+    optimizer,
+    scheduler,
+    metrics,
+    model_name,
+):
+    best_model_weights = None
+    best_mean_clustering_error = 1  # 1 represents 100% mean clustering error
+    for epoch in tqdm(range(config["full_epochs"]), desc="Training Full Model"):
+        model.train()
+        model = model.to(device)
+        epoch_loss_stage2 = 0.0
+        num_seq_processed = 0
+        w_info_sum = 0.0
+        w_res_sum = 0.0
+        w_feat_sum = 0.0
+        for batch_data in train_loader:
+            seq = batch_data["trajectories"].to(device).squeeze(0)  # (P, F, 2)
+            seq_t = batch_data["times"].to(device).squeeze(0)  # (P, F)
+            num_points = seq.shape[0]
+            
+            optimizer.zero_grad()
+            seq_aug1 = randomly_augment_seq(seq)
+            seq_aug2 = randomly_augment_seq(seq)
+            
+            mask1 = (torch.rand_like(seq_aug1[..., :1], device=device) > 0.25)
+            mask2 = (torch.rand_like(seq_aug2[..., :1], device=device) > 0.25)
+            
+            seq_aug1 = seq_aug1 * mask1
+            seq_aug2 = seq_aug2 * mask2
+            
+
+            f1, B1, h_t1 = model(seq_aug1, seq_t)
+            f2, B2, h_t2 = model(seq_aug2, seq_t)
+
+
+            B_flat1 = B1.view(num_points, -1)  # (P, 2F*rank)
+            B_flat2 = B2.view(num_points, -1)  # (P, 2F*rank)
+
+            v1 = torch.cat((f1, B_flat1), dim=1)
+            v2 = torch.cat((f2, B_flat2), dim=1)
+
+            v_norm1 = F.normalize(v1, p=2, dim=1)
+            v_norm2 = F.normalize(v2, p=2, dim=1)
+
+            x_reconstructed2 = reconstruct_x(seq_aug2, B2)  # (P, F, 2)
+            x_reconstructed_permuted2 = x_reconstructed2.permute(0, 2, 1)
+
+            loss_residual = L_Residual(
+                x_original=seq_aug2, x_reconstructed=x_reconstructed2
+            )
+
+            f_reconstructed2 = model.feature_extractor(x_reconstructed_permuted2)
+            loss_infoNCE = L_InfoNCE_unsupervised(v_norm1, v_norm2)
+
+            loss_featdiff = L_FeatDiff(
+                f_original=f1, f_reconstructed=f_reconstructed2
+            )
+
+            w_info = config["w_info"]
+            w_res = config["w_res"]
+            w_feat = config["w_feat"]
+
+            w_info_sum += w_info * loss_infoNCE
+            w_res_sum += w_res * loss_residual
+            w_feat_sum += w_feat * loss_featdiff
+
+            total_loss = (
+                w_info * loss_infoNCE
+                + w_res * loss_residual
+                + w_feat * loss_featdiff
+            )
+            total_loss.backward()
+            optimizer.step()
+            epoch_loss_stage2 += total_loss.item()
+            num_seq_processed += 1
+
+        scheduler.step()
+
+        metrics["total_loss"] = epoch_loss_stage2 / num_seq_processed
+        metrics["infonce_loss"] = w_info_sum / num_seq_processed
+        metrics["residual_loss"] = w_res_sum / num_seq_processed
+        metrics["feat_diff_loss"] = w_feat_sum / num_seq_processed
+
+        metrics["mean_clustering_error"] = evaluate_model_performance(
+            model, val_loader, config["train_data"], model_name=model_name
+        )
+        if additional_val_loader:
+            metrics[
+                f"{str.lower(config['additional_val_data'])}_mean_clustering_error"
+            ] = evaluate_model_performance(
+                model, additional_val_loader, config["additional_val_data"]
+            )
+
+        if metrics["mean_clustering_error"] < best_mean_clustering_error:
+            best_model_weights = copy.deepcopy(model.state_dict())
+        if wandb.run:
+            wandb.log(metrics, step=epoch + 1)
+
+        print(
+            f"Full Training Epoch {epoch + 1}/{config['full_epochs']}, Avg Loss: {metrics['total_loss']:.4f}"
+        )
+        print(
+            f"Epoch {epoch + 1}/{config['full_epochs']}: InfoNCE Loss: {metrics['infonce_loss']:.4f}, Res Loss: {metrics['residual_loss']:.4f}, Feat Loss: {metrics['feat_diff_loss']:.4f}"
+        )
+
+    model.load_state_dict(best_model_weights)
+    evaluate_model_performance(
+        model,
+        val_loader,
+        config["train_data"],
+        generate_video=config["generate_video_from_last_val_run"],
+        model_name=model_name,
+    )
+    return model
+
+
+def reconstruct_x(x_original, B_estimated):
+    with torch.no_grad():
+        try:
+            batch_size, seq_len, _ = x_original.shape
+            x_flattend = x_original.reshape(batch_size, 2 * seq_len, 1)
+            solution = torch.linalg.lstsq(B_estimated, x_flattend)
+            c = solution.solution
+
+            x_reconst_flat = torch.bmm(B_estimated, c)
+            x_reconst = x_reconst_flat.reshape(batch_size, seq_len, 2)
+        except Exception as e:
+            print(f"Error occurred in x reconstruction: {e}")
+    return x_reconst
+
+
+def randomize_sequences_for_class(seq_x, seq_labels, epoch, device):
+    generator = torch.Generator(device=device)
+    generator.manual_seed(42 + epoch)
+
+    seq_x_train = seq_x.clone()
+
+    if torch.rand(1, generator=generator, device=device) > 0.5:
+        unique_labels = torch.unique(seq_labels)
+        for label in unique_labels:
+            class_indices = seq_labels == label
+            if class_indices.sum() > 1:
+                seq_class = seq_x[class_indices]
+                num_seq_class = seq_class.size(0)
+                idx = torch.randperm(num_seq_class, generator=generator, device=device)
+                seq_class_shuffled = seq_class[idx]
+                seq_x_train[class_indices] = seq_class_shuffled
+
+    return seq_x_train
